52472db9514d33782e740630916f3e256c80d399
Lee-Sang-Beom <tkdqja975@naver.com>
2022-02-07 19:18:08 +0900

    add use localStorage, remember username

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const loginInput = document.querySelector(".login-form > input");
-const loginButton = document.querySelector(".login-form > button");
-const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector("#login-form div input");
+const loginButton = document.querySelector("#login-form div button");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASS);
     const username = loginInput.value;
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit)
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}
+
+else{
+    paintGreeting(savedUsername);
+}
