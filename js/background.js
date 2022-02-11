8540573fd41c175b4620319b0753affe1efa9512
Lee-Sang-Beom <tkdqja975@naver.com>
2022-02-11 23:37:49 +0900

    fixed error todolist

--- js/background.js
+++ js/background.js
@@ -17,4 +17,4 @@ const chosenImage = images[randomNumber];
 const bgImage = document.createElement("img");
 const bodyBackground = document.body.prepend(bgImage);
 
-bgImage.src = `/img/${chosenImage}`;
+bgImage.src = `./img/${chosenImage}`;
