a4f9803c140c66ae1787cb8fb4f28b54c1f68dd0
이상범 <tkdqja975@naver.com>
2023-01-17 11:50:57 +0900

    test

--- js/greetings.js
+++ js/greetings.js
@@ -2,54 +2,57 @@ const loginInput = document.querySelector("#login-form div input");
 const loginButton = document.querySelector("#login-form div button");
 const loginForm = document.querySelector("#login-form");
 
-
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASS);
-    const username = loginInput.value;
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASS);
+  const username = loginInput.value;
 
-    // 로컬 스토리지에 username key, username value 기록
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreeting(username);
+  // 로컬 스토리지에 username key, username value 기록
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
-function paintGreeting(username){
-    const quote = document.querySelector("#quotes span#quote");
-    const author = document.querySelector("#quotes span#author");
-    const greeting = document.querySelector("#greeting");
-    const todoForm = document.querySelector("#todo-form");
-    const linkLogo = document.querySelector("#link-icon");
-    const ball = document.querySelector(".ball");
-    const body = document.body;
+function paintGreeting(username) {
+  const quote = document.querySelector("#quotes span#quote");
+  const author = document.querySelector("#quotes span#author");
+  const greeting = document.querySelector("#greeting");
+  const todoForm = document.querySelector("#todo-form");
+  const linkLogo = document.querySelector("#link-icon");
+  const ball = document.querySelector(".ball");
+  const body = document.body;
+  const todoFormInput = document.querySelector("#todo-form-input");
+  const todoFormBtn = document.querySelector("#todo-form-btn");
 
-    // main greeing page greeting
-    greeting.innerText = `Welcome '${username}'`;
+  // debug test 2023-01-17
+  console.log(todoFormInput);
+  console.log(todoFormBtn);
 
-    // remove unnecessary tag
-    greeting.classList.remove(HIDDEN_CLASS);
-    todoForm.classList.remove(HIDDEN_CLASS);
-    linkLogo.classList.remove(HIDDEN_CLASS);
-    quote.classList.remove(HIDDEN_CLASS);
-    author.classList.remove(HIDDEN_CLASS);
-    body.classList.remove(HIDDEN_CLASS);
+  // main greeing page greeting
+  greeting.innerText = `Welcome '${username}'`;
 
-    ball.classList.add(HIDDEN_CLASS);
+  // remove unnecessary tag
+  greeting.classList.remove(HIDDEN_CLASS);
+  todoForm.classList.remove(HIDDEN_CLASS);
+  linkLogo.classList.remove(HIDDEN_CLASS);
+  quote.classList.remove(HIDDEN_CLASS);
+  author.classList.remove(HIDDEN_CLASS);
+  body.classList.remove(HIDDEN_CLASS);
+  todoFormBtn.classList.remove(HIDDEN_CLASS);
+  todoFormInput.classList.remove(HIDDEN_CLASS);
+  ball.classList.add(HIDDEN_CLASS);
 
-    document.querySelector("h1#clock").classList.remove(HIDDEN_CLASS);
+  document.querySelector("h1#clock").classList.remove(HIDDEN_CLASS);
 }
 
 // 로컬 스토리지에 username key에 대한 value 요청
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASS);
-    loginForm.addEventListener("submit",onLoginSubmit);
-}
-
-else{
-    paintGreeting(savedUsername);
-    
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
 }
--- js/weather.js
+++ js/weather.js
@@ -1,38 +1,39 @@
 const API_KEY = "82e4a5c487823316298a5347fe8263e7";
 
+function onGEOSuceess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
 
-function onGEOSuceess(position){
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then((res) => {
+    // Get information about the weather from the API
+    res.json().then((data) => {
+      const name = data.name;
+      const weather = data.weather[0].main;
+      const temp = data.main.temp;
+      const humidity = data.main.humidity;
+      const country = data.sys.country;
 
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+      const elementName = document.querySelector("div#weather div#name");
+      const elementWeather = document.querySelector(
+        "div#weather div#current-weather"
+      );
+      const elementTemp = document.querySelector("div#weather div#temperature");
+      const elementHumidity = document.querySelector(
+        "div#weather div#humidity"
+      );
+      const elementCountry = document.querySelector("div#weather div#country");
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then((res)=>{
-        // Get information about the weather from the API
-        res.json().then((data)=>{
-            const name = data.name;
-            const weather = data.weather[0].main;
-            const temp = data.main.temp;
-            const humidity = data.main.humidity
-            const country = data.sys.country;
-
-            const elementName = document.querySelector("div#weather div#name");
-            const elementWeather = document.querySelector("div#weather div#current-weather");
-            const elementTemp = document.querySelector("div#weather div#temperature");
-            const elementHumidity = document.querySelector("div#weather div#humidity");
-            const elementCountry = document.querySelector("div#weather div#country");
-
-            elementName.innerHTML = `지역 : ${country}, ${name}`;
-            elementWeather.innerHTML = `날씨 : ${weather}`;
-            elementTemp.innerHTML = `현재 온도 : ${temp}℃`;
-            elementHumidity.innerHTML = `습도 : ${humidity}`;
-        })
+      elementName.innerHTML = `지역 : ${country}, ${name}`;
+      elementWeather.innerHTML = `날씨 : ${weather}`;
+      elementTemp.innerHTML = `현재 온도 : ${temp}℃`;
+      elementHumidity.innerHTML = `습도 : ${humidity}`;
     });
-
+  });
 }
 
-function onGEOError(){
-    alert("Can't find you. No weather for you");
+function onGEOError() {
+  alert("Can't find you. No weather for you");
 }
 
-navigator.geolocation.getCurrentPosition(onGEOSuceess,onGEOError)
+navigator.geolocation.getCurrentPosition(onGEOSuceess, onGEOError);
